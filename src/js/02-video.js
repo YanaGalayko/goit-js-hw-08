ca2c7b6a610906031a607c668f3fa5fa063d2826
YanaGalayko <117485236+YanaGalayko@users.noreply.github.com>
2023-06-28 16:46:58 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,26 @@
 import Player from '@vimeo/player';
-console.log(Player);
+import throttle from 'lodash.throttle'
+
+// console.log(throttle);
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const onPlay = function(data) {
+    localStorage.setItem("videoplayer-current-time", data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem("videoplayer-current-time")
+console.log(currentTime);
+
+player.setCurrentTime(currentTime).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
