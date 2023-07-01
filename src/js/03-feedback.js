b72d98056e2d528aa8b05033ecdec33a10143891
YanaGalayko <117485236+YanaGalayko@users.noreply.github.com>
2023-07-01 19:54:26 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,12 @@
-// Відстежуй на формі подію input, і щоразу записуй у локальне сховище об'єкт з полями email і message,
-// у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
-// Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, заповнюй ними поля форми.
-// В іншому випадку поля повинні бути порожніми.
-// Під час сабміту форми очищуй сховище і поля форми, а також виводь у консоль об'єкт з полями email, message 
-// а їхніми поточними значеннями.
-// Зроби так, щоб сховище оновлювалось не частіше, ніж раз на 500 мілісекунд. Для цього додай до проекту і
-// використовуй бібліотеку lodash.throttle.
-
 import throttle from 'lodash.throttle'
 
 const LS_KEY = 'feedback-form-state';
 
-formEl = document.querySelector('.feedback-form');
+const form = document.querySelector('.feedback-form');
+console.log(form);
 
-formEl.addEventListener('input', throttle(inputData, 500));
-formEl.addEventListener('submit', submitForm);
+form.addEventListener('input', throttle(inputData, 500));
+form.addEventListener('submit', submitForm);
 
 let dataForm = JSON.parse(localStorage.getItem(LS_KEY)) || {};
 const { email, message } = form.elements;
